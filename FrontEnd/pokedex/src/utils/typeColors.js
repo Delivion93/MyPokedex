a6a5effa45de335f2f1acaa9e67c2252104b6d06
delivion93 <artem.abramov.1993@gmail.com>
2026-06-16 11:17:22 +0200

    feat: utils

---
+++ FrontEnd/pokedex/src/utils/typeColors.js
@@ -0,0 +1,37 @@
+export const typeColors = {
+  normal: "bg-gray-400 text-black",
+
+  fire: "bg-red-500 text-white",
+
+  water: "bg-blue-500 text-white",
+
+  electric: "bg-yellow-400 text-black",
+
+  grass: "bg-green-500 text-white",
+
+  ice: "bg-cyan-300 text-black",
+
+  fighting: "bg-orange-700 text-white",
+
+  poison: "bg-purple-500 text-white",
+
+  ground: "bg-amber-600 text-white",
+
+  flying: "bg-sky-400 text-black",
+
+  psychic: "bg-pink-500 text-white",
+
+  bug: "bg-lime-500 text-black",
+
+  rock: "bg-stone-500 text-white",
+
+  ghost: "bg-violet-700 text-white",
+
+  dragon: "bg-indigo-600 text-white",
+
+  dark: "bg-gray-800 text-white",
+
+  steel: "bg-slate-400 text-black",
+
+  fairy: "bg-pink-300 text-black",
+};
